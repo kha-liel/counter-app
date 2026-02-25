9dc6dc42e82ccbbfbd98a968a6fe2c37f6981ba7
Khaliel Myrie <khalamyrie@gmail.com>
2026-02-24 20:00:39 -0500

    trying different logics for color changing range

--- counter-app.js
+++ counter-app.js
@@ -33,6 +33,8 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         "/../",
     });
     this.count = 0;
+    this.min = 0;
+    this.max = 25;
   }
 
   // Lit reactive properties
@@ -41,8 +43,10 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
       ...super.properties,
       title: { type: String },
       count: { type: Number, reflect: true},
-    };
-  }
+      min: { type: Number, reflect: true},
+      max: { type: Number, reflect: true}
+      }};
+  
 
   // Lit scoped styles
   static get styles() {
@@ -50,12 +54,9 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     css`
       :host {
         display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
       }
 
-      :host([count="18"]) h3 {
+      /*:host([count="18"]) h3 {
         color: var(--ddd-them-default-original87Pink);
       }
       :host([count="21"]) h3 {
@@ -64,29 +65,64 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
       :host([count="0"]) h3 {
         color: var(--ddd-theme-default-creamyPeach);
       }
-
-    :host([count="25"]) h3 {
-      color: var(--ddd-theme-default-pennBlue);
-    }
+      :host([count="25"]) h3 {
+        color: var(--ddd-theme-default-pennBlue);
+      }*/
       
       .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: var(--ddd-spacing-4);
+        padding: var(--ddd-spacing-8);
       }
-      h3 span {
-        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+
+      .counter {
+        margin: 0;
+        font-size: var(--ddd-font-size-xxl);
+        font-family: var(--ddd-font-navigation);
+        line-height: 1;
+        padding-bottom: var(--ddd-spacing-4);
+      }
+
+      .button-container {
+        display: flex;
+        gap: var(--ddd-spacing-4);
+      }
+
+      button {
+        padding: var(--ddd-spacing-2) var(--ddd-spacing-4);
+        font-size: var(--ddd-font-size-ms);
+        border: var(--ddd-border-sm);
+        background-color: var(--ddd-theme-default-limestone);
+        border-radius: var(--ddd-radisu-xs);
+      }
+      
+      button:hover, button:focus {
+        background-color: var(--ddd-theme-default-alertImmediate);
+        color: white;
+        outline: none;
+        box-shadow: var(--ddd-boxShadow-sm);
       }
     `];
   }
+
+counterColor() {
+  if (this.count === 18) return 'var(--ddd-theme-default-Original87Pink)';
+  if (this.count === 21) return 'var(--ddd-theme-default-keppel)';
+  if (this.count === this.min) return 'var(--ddd-theme-default-creamyPeach)';
+  if (this.count === this.max) return 'var(--ddd-theme-default-pennBlue)';
+  return inherit;
+}
 
   // Lit render the HTML
   render() {
     return html`
 <div class="wrapper">
-  <h3>${this.count}</h3>
+  <div class="counter" style = "color: ${this.counterColor}">${this.count}</div>
   <div class="button-container">
-    <button @click="${this.decrement}" ?disabled="${this.count === 0}">-</button>
-    <button @click="${this.increment}" ?disabled="${this.count === 25}">+</button>
+    <button @click="${this.decrement}" ?disabled="${this.count === this.min}">-</button>
+    <button @click="${this.increment}" ?disabled="${this.count === this.max}">+</button>
   </div>
   <slot></slot>
 </div>`;
